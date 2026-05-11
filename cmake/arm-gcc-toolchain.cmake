d38254c6d62d3eb53245f27fc6530b33c97f066b
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-05-11 18:18:00 -0400

    Update ARM GCC toolchain file for MCU handling

--- cmake/arm-gcc-toolchain.cmake
+++ cmake/arm-gcc-toolchain.cmake
@@ -1,13 +1,16 @@
 # MIT License
 # AeroCam ARM GCC Toolchain File
-# Supports STM32F4 / F7 / H7 (compile-time selectable)
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # -------------------------------------------------------------------
-# Compiler Paths
+# Expect: -DMCU=F4 / F7 / H7
 # -------------------------------------------------------------------
+if(NOT DEFINED MCU)
+    message(FATAL_ERROR "Define -DMCU=F4, F7, or H7 when configuring.")
+endif()
+
 set(ARM_GCC_PREFIX arm-none-eabi-)
 
 set(CMAKE_C_COMPILER   ${ARM_GCC_PREFIX}gcc)
@@ -19,21 +22,18 @@ set(CMAKE_OBJDUMP      ${ARM_GCC_PREFIX}objdump)
 set(CMAKE_SIZE         ${ARM_GCC_PREFIX}size)
 
 # -------------------------------------------------------------------
-# CPU Selection
+# CPU flags based on MCU
 # -------------------------------------------------------------------
-if(MCU_F4)
+if(MCU STREQUAL "F4")
     set(CPU_FLAGS "-mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard")
-elseif(MCU_F7)
+elseif(MCU STREQUAL "F7")
     set(CPU_FLAGS "-mcpu=cortex-m7 -mfpu=fpv5-sp-d16 -mfloat-abi=hard")
-elseif(MCU_H7)
+elseif(MCU STREQUAL "H7")
     set(CPU_FLAGS "-mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard")
 else()
-    message(FATAL_ERROR "No MCU family defined. Use -DMCU_F4, -DMCU_F7, or -DMCU_H7.")
+    message(FATAL_ERROR "Unknown MCU: ${MCU}. Use F4, F7, or H7.")
 endif()
 
-# -------------------------------------------------------------------
-# Common Flags
-# -------------------------------------------------------------------
 set(COMMON_FLAGS "
     ${CPU_FLAGS}
     -mthumb
@@ -43,15 +43,12 @@ set(COMMON_FLAGS "
     -fno-rtti
     -fdata-sections
     -ffunction-sections
-    -Wall -Wextra -Werror
+    -Wall -Wextra
 ")
 
 set(CMAKE_C_FLAGS   "${COMMON_FLAGS} -std=c11")
 set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=c++20")
 
-# -------------------------------------------------------------------
-# Linker
-# -------------------------------------------------------------------
 set(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/AeroCam.ld)
 
 set(CMAKE_EXE_LINKER_FLAGS "
