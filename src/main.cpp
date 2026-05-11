65a5978130229dec8522fc9c3f04b84cf71076c7
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-05-11 17:53:30 -0400

    Implement STM32 system clock configs

--- src/main.cpp
+++ src/main.cpp
@@ -148,9 +148,41 @@ static MotorDriver* g_motors = nullptr;
 // MCU-SPECIFIC INITIALIZATION BLOCKS
 // ============================================================
 
+[[noreturn]] static void ClockConfig_ErrorHandler() {
+    __disable_irq();
+    while (true) {
+    }
+}
+
 #if defined(MCU_F4)
 static void SystemClock_Config() {
-    // F4 PLL config
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitTypeDef osc = {};
+    osc.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    osc.HSIState = RCC_HSI_ON;
+    osc.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    osc.PLL.PLLState = RCC_PLL_ON;
+    osc.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    osc.PLL.PLLM = 8;
+    osc.PLL.PLLN = 168;
+    osc.PLL.PLLP = RCC_PLLP_DIV2;
+    osc.PLL.PLLQ = 7;
+    if (HAL_RCC_OscConfig(&osc) != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
+
+    RCC_ClkInitTypeDef clk = {};
+    clk.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                    RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    clk.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    clk.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    clk.APB1CLKDivider = RCC_HCLK_DIV4;
+    clk.APB2CLKDivider = RCC_HCLK_DIV2;
+    if (HAL_RCC_ClockConfig(&clk, FLASH_LATENCY_5) != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
 }
 static void MX_SPI1_Init() {
     // SPI1 init for F4
@@ -164,7 +196,37 @@ static void MX_TIM2_Init() {
 
 #elif defined(MCU_F7)
 static void SystemClock_Config() {
-    // F7 PLL config
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitTypeDef osc = {};
+    osc.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    osc.HSIState = RCC_HSI_ON;
+    osc.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    osc.PLL.PLLState = RCC_PLL_ON;
+    osc.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    osc.PLL.PLLM = 8;
+    osc.PLL.PLLN = 216;
+    osc.PLL.PLLP = RCC_PLLP_DIV2;
+    osc.PLL.PLLQ = 9;
+    if (HAL_RCC_OscConfig(&osc) != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
+
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
+
+    RCC_ClkInitTypeDef clk = {};
+    clk.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                    RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    clk.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    clk.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    clk.APB1CLKDivider = RCC_HCLK_DIV4;
+    clk.APB2CLKDivider = RCC_HCLK_DIV2;
+    if (HAL_RCC_ClockConfig(&clk, FLASH_LATENCY_7) != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
 }
 static void MX_SPI1_Init() {
     // SPI1 init for F7
@@ -178,7 +240,46 @@ static void MX_TIM2_Init() {
 
 #elif defined(MCU_H7)
 static void SystemClock_Config() {
-    // H7 PLL config
+    if (HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY) != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
+
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+    }
+
+    RCC_OscInitTypeDef osc = {};
+    osc.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    osc.HSIState = RCC_HSI_DIV1;
+    osc.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    osc.PLL.PLLState = RCC_PLL_ON;
+    osc.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    osc.PLL.PLLM = 4;
+    osc.PLL.PLLN = 50;
+    osc.PLL.PLLP = 2;
+    osc.PLL.PLLQ = 4;
+    osc.PLL.PLLR = 2;
+    osc.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+    osc.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+    osc.PLL.PLLFRACN = 0;
+    if (HAL_RCC_OscConfig(&osc) != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
+
+    RCC_ClkInitTypeDef clk = {};
+    clk.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                    RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 |
+                    RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1;
+    clk.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    clk.SYSCLKDivider = RCC_SYSCLK_DIV1;
+    clk.AHBCLKDivider = RCC_HCLK_DIV2;
+    clk.APB3CLKDivider = RCC_APB3_DIV2;
+    clk.APB1CLKDivider = RCC_APB1_DIV2;
+    clk.APB2CLKDivider = RCC_APB2_DIV2;
+    clk.APB4CLKDivider = RCC_APB4_DIV2;
+    if (HAL_RCC_ClockConfig(&clk, FLASH_LATENCY_4) != HAL_OK) {
+        ClockConfig_ErrorHandler();
+    }
 }
 static void MX_SPI1_Init() {
     // SPI1 init for H7
